9b413e48895f110025db5352964bcae1a7061572
Emmanuel Mancilla <emmancillaa@gmail.com>
2022-05-25 15:17:27 -0300

    primera entrega del proyecto

--- script.js
+++ script.js
@@ -1,20 +1,66 @@
-//declarando variables
 solicitarNombre ();
-let precio= -1;
-let iva;
-let final;
+let car=0;
+
+
+
+const autos = [
+    {
+        nombre: "Ford Fiesta",
+        ano: 2018,
+        precio: "10.000 USD",
+        id: 1
+    },
+        {
+        nombre: "Gol Trend",
+        ano: 2017,
+        precio: "7.000 USD",
+        id: 2
+        },
+
+        {
+            nombre: "Honda Civic",
+            ano: 2020,
+            precio: "15.000 USD",
+            id: 3
+            }
+
+
+
+]
+
+
+
+
 do {
 
-    precio=prompt("Ingresar precio del producto")
-    if (precio<0 || isNaN(precio))  {
+    car=prompt("Para ver los autos en stock presiona 1, 2 o 3")
+    if (car<=0 || isNaN(car) || car>3)  {
         alert ("Ingresar numeros validos")
     }
-} while (precio<0 || isNaN(precio)==true)
+} while (car<=0 || isNaN(car))
 
-iva = precio*0.21;
-final= Number(precio)+Number(iva);
-alert ("El iva del producto es "+iva )
-alert ("El precio final del producto teniendo en cuenta el iva es "+final)
+let found = autos.find(autos => autos.id == car);
+
+console.log(found);
+if (found.id == 1)
+{
+    alert("El auto seleccionado es "+found.nombre)
+    alert("Hecho el año "+found.ano)
+    alert("Con un precio de "+found.precio)
+}
+if (found.id == 2)
+{
+    alert("El auto seleccionado es "+found.nombre)
+    alert("Hecho el año "+found.ano)
+    alert("Con un precio de "+found.precio)
+}
+if (found.id == 3)
+{
+    alert("El auto seleccionado es "+found.nombre)
+    alert("Hecho el año "+found.ano)
+    alert("Con un precio de "+found.precio)
+}
+
 
 
 function solicitarNombre () {
